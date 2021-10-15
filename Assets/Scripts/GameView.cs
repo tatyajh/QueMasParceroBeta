81fdb6b8e576744f760e2d2321e21a74af4aafa4
tatyajh <talejandra.jaramillo@gmail.com>
2021-10-14 23:33:11 -0500

    Menu

---
+++ Assets/Scripts/GameView.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameView : MonoBehaviour {
+
+    public Text empanadaText, scoreText, maxScoreText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(GameManager.sharedInstance.currentGameState == GameState.inGame){
+            int empanadas = 0;
+            float score = 0;
+            float maxScore = 0;
+
+            empanadaText.text = empanadas.ToString();
+            scoreText.text = "Score: " + score.ToString("f1");
+            maxScoreText.text = "MaxScore: " + maxScore.ToString("f1");
+        }
+	}
+}
